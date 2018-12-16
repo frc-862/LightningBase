525d233be7db01db2bad4c543cbe938b4ba44f7a
Patrick Hurley <phurley@gmail.com>
2018-12-15 21:37:09 -0500

    Base code pulled in from previous years, with new logging cleanup

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,8 @@
+package frc.robot;
+
+import frc.lightning.ConstantBase;
+
+public class Constants extends ConstantBase {    
+	public static int leftThrottleAxis = 1;
+    public static int rightThrottleAxis = 3;    
+}
---
+++ src/main/java/frc/robot/OI.java
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.robot.commands.Shoot;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+  private Joystick driver = new Joystick(0);
+  private Button shootButton = new JoystickButton(driver, RobotMap.shootButton);
+
+  public double getLeftPower() { 
+    return -driver.getRawAxis(Constants.leftThrottleAxis);
+  }
+
+  public double getRightPower() {
+    return -driver.getRawAxis(Constants.rightThrottleAxis);
+  }
+  
+  public OI() {
+    shootButton.whenPressed(new Shoot());
+  }
+}
