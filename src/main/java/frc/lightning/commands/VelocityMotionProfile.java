ac0d2978455d54dbe7ba4b490b0bbe21346e09df
Patrick Hurley <phurley@gmail.com>
2019-01-23 18:14:20 -0500

    Add some logging to example motion profile command

--- src/main/java/frc/lightning/commands/VelocityMotionProfile.java
+++ src/main/java/frc/lightning/commands/VelocityMotionProfile.java
@@ -1,9 +1,12 @@
 package frc.lightning.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.lightning.logging.CommandLogger;
 import frc.robot.Robot;
 
 public class VelocityMotionProfile extends Command {
+    CommandLogger logger = new CommandLogger(getClass().getCanonicalName());
+
     protected double[][] leftPath;
     protected double[][] rightPath;
     private int index = 0;
@@ -12,6 +15,19 @@ public class VelocityMotionProfile extends Command {
         requires(Robot.drivetrain);
         leftPath = left;
         rightPath = right;
+
+        logger.addDataElement("expectedLeft");
+        logger.addDataElement("expectedRight");
+        logger.addDataElement("actualLeft");
+        logger.addDataElement("actualRight");
+
+        logger.addDataElement("expectedLeftVelocity");
+        logger.addDataElement("expectedRightVelocity");
+        logger.addDataElement("actualLeftVelocity");
+        logger.addDataElement("actualRightVelocity");
+
+        logger.addDataElement("expectedTheta");
+        logger.addDataElement("actualTheta");
     }
 
     /**
@@ -28,10 +44,15 @@ public class VelocityMotionProfile extends Command {
     final double kA = 0.0;
     final double kTheta = 0.0;
 
-    private double calcBaseVelocity(double[] point, double dist) {
-        double error = point[0] - Robot.drivetrain.getLeftDistance();
+    private double calcBaseVelocity(String tag, double[] point, double dist) {
+        double error = point[0] - dist;
         double velocity = point[1];
         double acceleration = point[2];
+
+        logger.set("expected" + tag, point[0]);
+        logger.set("actual" + tag, dist);
+        logger.set("expected" + tag + "Velocity", velocity);
+
         return velocity + error * kP + acceleration * kA;
     }
 
@@ -41,13 +62,23 @@ public class VelocityMotionProfile extends Command {
      */
     @Override
     protected void execute() {
-        final double leftVel = calcBaseVelocity(leftPath[index], Robot.drivetrain.getLeftDistance());
-        final double rightVel = calcBaseVelocity(rightPath[index], Robot.drivetrain.getRightDistance());
+        if (index < leftPath.length && index < rightPath.length) {
+            final double leftVel = calcBaseVelocity("Left", leftPath[index], Robot.drivetrain.getLeftDistance());
+            final double rightVel = calcBaseVelocity("Right", rightPath[index], Robot.drivetrain.getRightDistance());
 
-        final double thetaError = leftPath[index][3] - Robot.core.getHeading();
-        final double turn = thetaError * kTheta;
+            logger.set("actualLeftVelocity", Robot.drivetrain.getLeftVelocity());
+            logger.set("actualRightVelocity", Robot.drivetrain.getRightVelocity());
 
-        Robot.drivetrain.setVelocity(leftVel - turn, rightVel + turn);
+            final double thetaError = leftPath[index][3] - Robot.core.getHeading();
+            final double turn = thetaError * kTheta;
+            logger.set("expectedTheta", leftPath[index][3]);
+            logger.set("expectedTheta", Robot.core.getHeading());
+
+            Robot.drivetrain.setVelocity(leftVel - turn, rightVel + turn);
+
+            logger.writeValues();
+            index += 1;
+        }
     }
 
 
@@ -63,6 +94,8 @@ public class VelocityMotionProfile extends Command {
      */
     @Override
     protected void end() {
+        logger.drain();
+        logger.flush();
         Robot.drivetrain.stop();
     }
 }
